7d8ffe9bafd6ceb9c8e037cc57dee43b6398f55c
EidHachem <eidhachem1@gmail.com>
2022-10-18 22:00:27 +0300

    Move loader import to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import LeagueDetails from './Components/LeagueDetails';
 import Main from './Components/Main';
 import LeaguesList from './Components/LeaguesList';
+import Loader from './Components/Loader';
+import './index.css';
 
 function App() {
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 3000);
+  }, [loader]);
+
+  if (loader) {
+    return (
+      <div className="loader">
+        <Loader />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Routes>
